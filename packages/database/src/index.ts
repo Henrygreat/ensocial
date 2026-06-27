64599e44b987668d91425e14dc1e467d67ba4af3
Henry Great <oyaincrease@gmail.com>
2026-06-27 14:05:39 +0100

    Fix Prisma client imports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
@@ -8,7 +8,10 @@ export const prisma =
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
-export * from '../generated/client';
+export * from '@prisma/client';
+export { PrismaClient };
 export { prisma as db };
